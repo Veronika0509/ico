5fc93136748e7750222bca2bca83338262baa99d
veronika0509 <veronikachika0509@gmail.com>
2022-04-10 20:16:38 +0300

    Changes by yuriylebid

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,18 @@ import {Language} from "./components/start-page";
 import {Button} from "./components/start-page";
 
 export const App: React.FC = () => {
+    const [language, setLanguage] = React.useState(navigator.language === 'ru' ? 'ru' : 'en')
+
+    const [walletConnected, setWalletConnected] = React.useState(false)
+    const [admin, setAdmin] = React.useState(true)
+
+    const buttonClickHandle = () => {
+        // todo
+        console.log('clicked')
+        setWalletConnected(true)
+        setAdmin(true)
+    }
+
     return (
         <>
             { walletConnected || (
